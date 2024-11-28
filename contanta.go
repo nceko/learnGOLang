4a8e01066e917436d91b02c2a81ece88405681b3
Eko Hidayat <nc.ekohidayat@gmail.com>
2024-11-28 21:28:52 +0700

    Feat: Penerapan const

---
+++ contanta.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	const awalName = "Eko"
+	const endName = "Hidayat"
+	fmt.Println(name)
+
+	// awalName = "Hidayat"
+	// awalName = "Hidayat"
+	// fmt.Println(name)
+}
