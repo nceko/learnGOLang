93a24febe8747946975b8b645b4aeb6d0a7a2fd0
Eko Hidayat <nc.ekohidayat@gmail.com>
2024-11-28 21:46:29 +0700

    Feat: Operation mat pada golang

---
+++ operation_math.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var (
+		a = 10
+		b = 4
+	)
+
+	// Penjumlahan
+	c := a + b
+	fmt.Println(c)
+
+	// Pengurangan
+	d := a - b
+	fmt.Println(d)
+
+	// Perkalian
+	e := a * b
+	fmt.Println(e)
+
+	// Pembagian
+	f := a / b
+	fmt.Println(f)
+
+	// Modulus
+	g := a % b
+	fmt.Println(g)
+}
